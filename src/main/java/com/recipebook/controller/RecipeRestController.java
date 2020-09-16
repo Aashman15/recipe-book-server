52904bb97c4ca453faf5e6300c2c8ddae5997718
Aashman15 <aashmanthing1@gmail.com>
2020-09-16 12:34:44 +0545

    thymeleaf implemented

--- src/main/java/com/recipebook/controller/RecipeRestController.java
+++ src/main/java/com/recipebook/controller/RecipeRestController.java
@@ -3,24 +3,36 @@ package com.recipebook.controller;
 import com.recipebook.model.Recipe;
 import com.recipebook.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
+@Controller
 @CrossOrigin(origins = "http://localhost:4200")
 public class RecipeRestController {
     @Autowired
     RecipeRepository recipeRepository;
 
     @PutMapping("/saveRecipes")
+    @ResponseBody
     List<Recipe> deleteRecipesAndSaveRecipes(@RequestBody List<Recipe> recipes) {
         recipeRepository.deleteAll();
         return recipeRepository.saveAll(recipes);
     }
 
     @GetMapping("/getRecipes")
+    @ResponseBody
     List<Recipe> getRecipes() {
         return recipeRepository.findAll();
+    }
+
+    @GetMapping("/home")
+    public ModelAndView getHome() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        modelAndView.addObject("response", "Hello World !!!");
+        return modelAndView;
     }
 }
